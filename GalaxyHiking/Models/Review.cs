134be5f4d54366c08d51a8bded5edbe2a121a6ed
briandyko <briandyko@gmail.com>
2017-10-28 16:41:45 -0400

    built review and category models

---
+++ GalaxyHiking/Models/Review.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GalaxyHiking.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Galaxy { get; set; }
+        public string Planet { get; set; }
+        public string PlanetType { get; set; }
+        public int AlienEncounters { get; set; }
+        public bool IsFriendly { get; set; }
+        public string Comments { get; set; }
+        public DateTime PostDate { get; set; }
+        public string Author { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
